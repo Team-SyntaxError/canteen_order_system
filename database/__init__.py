110164f3dcdbb7677d67399423b0df051e94a352
saiujwal chelamkuri <69893826+chsaiujwal@users.noreply.github.com>
2022-06-24 13:56:32 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -6,7 +6,7 @@ try:
     mongo_client = MongoClient("mongodb://demo:nj7XPPKsAYnXRC26@ac-yxm9bwo-shard-00-00.rxay9px.mongodb.net:27017,ac-yxm9bwo-shard-00-01.rxay9px.mongodb.net:27017,ac-yxm9bwo-shard-00-02.rxay9px.mongodb.net:27017/?ssl=true&replicaSet=atlas-b36efq-shard-0&authSource=admin&retryWrites=true&w=majority")
     mongo_client.server_info()
 except ConnectionFailure:
-    print("Invalid Mongo DB URL. Please Check Your Credentials! Friday is Exiting...")
+    print("Invalid Mongo DB URL. Please Check Your Credentials! Exiting...")
     quit(1)
 
 
