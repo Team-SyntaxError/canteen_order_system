0c3ed1c9f013f9106681048a2ae363d5c1856856
saiujwal chelamkuri <69893826+chsaiujwal@users.noreply.github.com>
2022-06-24 09:08:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import secrets
+import string
+from database.func import add
+import PySimpleGUI as sg
+
+def passgen():
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(alphabet) for i in range(60))
+sg.theme("DarkGreen")
+layout = [
+    [sg.Text('FC BVRIT', text_color="Red")],
+    [sg.Checkbox('PIZZA', default=False, key="PIZZA"),sg.Input(size=(10), key="pizza") ],
+    [sg.Button('BILL IT')]
+    [sg.Text('TOTAL: ', text_color="Red"), sg.Input("", key='-FINAL-')],
+
+    [sg.Cancel()],
+]
+window = sg.Window('FC ORDER SYSTEM', layout,icon=r'C:\Users\chsai\Desktop\folder_locker\enc.ico', size=(500, 500))
+# add(dic,password)
+while True:
+    event, values = window.read()
+    if event is None or event == 'Cancel':
+        break
+    if values["-OUTPUT-"]:
+        window['-FINAL-'].update(values["pizza"]*130)
